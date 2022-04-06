8912704283d50b248dac422da7cdea1920120dd3
Peter Sobolewski <pete.sd@gmail.com>
2022-04-06 21:39:50 +0200

    Add typing to test

--- stardist_napari/_tests/test_plugin.py
+++ stardist_napari/_tests/test_plugin.py
@@ -1,10 +1,14 @@
+from typing import Callable
+
 from stardist.data import test_image_nuclei_2d as image_nuclei_2d
 
 from stardist_napari._dock_widget import plugin_wrapper
 
 
-def test_run(make_napari_viewer):
+def test_run(make_napari_viewer: Callable[..., napari.Viewer]) -> None:
     viewer = make_napari_viewer()
     viewer.add_image(image_nuclei_2d())
     plugin = plugin_wrapper()
     viewer.window.add_dock_widget(plugin)
+    # Check widget was added
+    assert len(viewer.window._dock_widgets) == 1
